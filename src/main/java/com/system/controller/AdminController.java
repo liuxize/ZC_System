240797a92bad842e414d6424562edefa6d16da19
liuxize <liuxize95@gmail.com>
2018-12-20 14:38:33 +0800

    Revert "er"
    This reverts commit 6937a27dd890d85370e07c48b534a86e2535b6fa.

--- src/main/java/com/system/controller/AdminController.java
+++ src/main/java/com/system/controller/AdminController.java
@@ -2847,7 +2847,8 @@ public class AdminController {
     //利用cookie存储当前的进度
     @ResponseBody
     @RequestMapping(value = "/getProgressValue", method = {RequestMethod.POST})
-    public void getProgressValue(HttpServletRequest request, HttpServletResponse response){
+    public void getProgressValue(@RequestParam(value = "isComplated") String isComplated, HttpServletResponse response){
+
         //根据名字获取cookie
         //读取cookie
 //        String result = null;
@@ -2881,7 +2882,9 @@ public class AdminController {
 //        response.setCharacterEncoding("UTF-8");
 //        response.setContentType("text/html;charset=UTF-8");
 //        response.addCookie(div);
-
+        if(isComplated.equals("true")){
+            index=0;
+        }
         String json = "{\"progressValue\":\"" + index + "\"}";
         try{
             response.getWriter().print(json);  //返回json数据格式
